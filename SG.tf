ec9ceceb4bbac22aad10e6c7bef040df47d1b49b
Rajput7500 <132784995+milton7500@users.noreply.github.com>
2024-09-24 14:55:56 +0530

    Create SG.tf

---
+++ SG.tf
@@ -0,0 +1,45 @@
+####################################
+#Security Group#
+######################################
+
+resource "aws_security_group" "mysg" {
+  name        = "my-sg"
+  description = "allow ports ssh http tcp"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTPs"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "security groups"
+  }
+
+  depends_on = [aws_vpc.myvpc]
+
+}
