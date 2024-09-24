c7f8a3fbc76fdb971610b41c81160ad2993c7eb6
Rajput7500 <132784995+milton7500@users.noreply.github.com>
2024-09-24 15:08:28 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,10 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0ec0e125bb6c6e8ec"
   instance_type = "t2.micro"
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.myenic.id
-  }
 
   tags = {
     Name = "HelloWorld"
