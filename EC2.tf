e09c065450f291ed4e74e986708136e6113cf17d
Rajput7500 <132784995+milton7500@users.noreply.github.com>
2024-09-24 14:55:03 +0530

    Create EC2.tf

---
+++ EC2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0ec0e125bb6c6e8ec"
+  instance_type = "t2.micro"
+  key_name      = "test"
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.myenic.id
+  }
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
